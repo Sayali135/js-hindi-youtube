34e0f96da297c7be982a7b8ef0b13eb3eccdc94e
Sayali135 <sayaliwork1602@gmail.com>
2026-06-13 13:38:17 +0530

    datatypes

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,17 @@
+const accountId=12456
+let accountEmail="sayalideokar95@gmail.com"
+var accoutPass="Sayali@1602"
+accountCity="Pune"
+let accountState;
+// accountId=78945//not allowed
+accountEmail="rani@gmail.com"
+accoutPass="Sayali@1234"
+accountCity="Mumbai"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accoutPass,accountCity,accountState])
+/*
+prefer not to use var
+becuase use of block scope and functional scope 
+
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use script"; //it means all code trated as newer version and here whole file will be trated as new code
+//alert(3+2) //we are using nodejs and not browser
+console.log(3+2);
+//code readability should be high 
+console.log("sayali")
+// window.alert("Hello world!");
+// alert("Hello world!");
+
+let name ="sayali"
+let age=25
+let isLoggedIn=false
+//number like 2 raused to 5
+//bigint
+//string=""
+//boolean=true/false
+//null=standalone value like 25 "hello" withou declaring the datatype
+//undefined 
+//symbol=>it is for unique
+console.log(typeof "hitesh");
+console.log(typeof 22);
+console.log(typeof null);//object
+
+console.log(typeof undefined);//undefined
+
